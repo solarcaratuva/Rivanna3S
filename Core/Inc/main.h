c0e71778f535a01860d92a9f7c483d7131320713
DavidMe03 <davidmerino407@gmail.com>
2025-09-14 16:36:17 -0400

    Changed pinmap.h format

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -28,6 +28,7 @@ extern "C" {
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32h7xx_hal.h"
+#include "pinmap.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
