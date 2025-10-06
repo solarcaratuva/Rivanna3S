39e734282e53de3ae372cd1e5eb1b9579c573195
dt477 <domtr0510@gmail.com>
2025-10-06 12:47:40 -0400

    formatting and docstrings

--- Core/Inc/Clock.h
+++ Core/Inc/Clock.h
@@ -8,12 +8,23 @@
 class Clock {
 
 public:
-	Clock();
+	Clock(); // Constructor sets lastSleepSinceCall
+    /*
+     * Function that sleeps for time_ms since the last time_ms
+     */
     void sleep_since(uint32_t time_ms);
+
+    /*
+     * Function that sleeps for time_ms
+     */ 
     static void sleep_for(uint32_t time_ms);
+
+    /* 
+     * Function that returns the current time
+     */
     static uint32_t get_current_time();
 private:
-    TickType_t xLastSleepSinceCall;
+    TickType_t lastSleepSinceCall;
 };
 
 
--- Core/Src/Clock.cpp
+++ Core/Src/Clock.cpp
@@ -1,22 +1,22 @@
 #include "Clock.h"
 
 Clock::Clock() {
-    xLastSleepSinceCall = xTaskGetTickCount();
+    lastSleepSinceCall = xTaskGetTickCount();
 }
 
 void Clock::sleep_since(uint32_t time_ms) {
-    TickType_t tickCount = time_ms / portTICK_PERIOD_MS; // this converts the ms into ticks
-    vTaskDelayUntil(&xLastSleepSinceCall, tickCount); // xLastSleepSinceCall is updated here
+    TickType_t tick_count = time_ms / portTICK_PERIOD_MS; // this converts the ms into ticks
+    vTaskDelayUntil(&xLastSleepSinceCall, tick_count); // xLastSleepSinceCall is updated here
     return;
 }
 
 void Clock::sleep_for(uint32_t time_ms) {
-    const TickType_t xDelay = time_ms / portTICK_PERIOD_MS;
-    vTaskDelay( xDelay );
+    const TickType_t delay = time_ms / portTICK_PERIOD_MS;
+    vTaskDelay(delay);
     return;
 }
 
 uint32_t Clock::get_current_time() {
-    TickType_t tickCount = xTaskGetTickCount();
-    return static_cast<uint32_t>(tickCount * portTICK_PERIOD_MS); 
+    TickType_t tick_count = xTaskGetTickCount();
+    return static_cast<uint32_t>(tick_count * portTICK_PERIOD_MS); 
 }
