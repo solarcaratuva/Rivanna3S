5e898f6bfbc96e58a431722bc19189b413310ec0
TamerTimraz <tamertimraz04@gmail.com>
2025-11-25 21:06:00 -0500

    added generation for I2C and FDCAN peripherals

--- Drivers/code_generation/generate_peripheralmap.py
+++ Drivers/code_generation/generate_peripheralmap.py
@@ -1,3 +1,4 @@
+from sys import prefix
 import xml.etree.ElementTree as ET
 import os
 import re
@@ -5,9 +6,8 @@ from generate_pinmap import parse_XML_pinmap
 
 """ 
 TODO: 
-    - Add I2C, SPI, FDCAN peripheral arrays in cpp file.
-    - Add ADC peripheral mapping (currently excluded from peripheral map).
-    - Add I2C, SPI, FDCAN alternate function methods in cpp file.
+    - Add ADC peripheral mapping (currently excluded from peripheral map)
+    - Generate ADC and SPI peripheral arrays and get_<PERIPHERAL>_AF functions
 """
 
 # Define valid signals for each peripheral type (excludes ADC for now)
@@ -70,20 +70,186 @@ def parse_XML_alternate_functions(dir, peripheral_map):
 
     # Formatted as { "UART4": { "RX": { "PA0": AF#, "PA1": AF# }, "TX": { "PA2": AF#, "PA3": AF# } }, ... }
     af_map = {}
-    for map in peripheral_map.values():
+    for peripheral, map in peripheral_map.items():
+        af_map[peripheral] = {}
         for instance, values in map.items():
-            af_map[instance] = {}
+            af_map[peripheral][instance] = {}
             for mode, pins in values.items():
-                af_map[instance][mode] = {}
+                af_map[peripheral][instance][mode] = {}
                 for pin in pins:
                     for pin_entry in root.findall('{http://dummy.com}GPIO_Pin'):
                         if pin_entry.attrib['Name'] == pin:
                             for signal in pin_entry.findall('{http://dummy.com}PinSignal'):
                                 if signal.attrib['Name'] == instance + '_' + mode:
                                     af = signal[0][0].text
-                                    af_map[instance][mode][pin] = af
+                                    af_map[peripheral][instance][mode][pin] = af
     
     return af_map
+
+
+def write_af_arrays(f, af_map, peripheral_type):
+    for peripheral, modes in af_map[peripheral_type].items():
+        for mode, pins in modes.items():
+            if not pins:
+                continue
+
+            f.write(f"    AF_Info {peripheral}_{mode}[] = {{\n")
+            for pin_name, af in pins.items():
+                c_pin = pin_name[:2] + "_" + pin_name[2:]
+                f.write(f"        {{{c_pin}, {af}}},\n")
+            f.write("    };\n")
+            f.write(f"    uint8_t {peripheral}_{mode}_len = {len(pins)};\n\n")
+
+
+def write_get_uart_af(f, af_map):
+    f.write("uint8_t get_UART_AF(UART_HandleTypeDef* handle, Pin* pin, uint8_t mode) {\n")
+
+    write_af_arrays(f, af_map, "UART")
+    write_af_arrays(f, af_map, "USART")
+
+    f.write("    AF_Info* array;\n    uint8_t array_len;\n\n")
+
+    for peripheral in af_map.keys():
+        if peripheral == "UART" or peripheral == "USART":
+            for instance in af_map[peripheral].keys():
+                if af_map[peripheral][instance]:
+                    f.write(f"""
+    if(handle->Instance == {instance}) {{
+        if(mode == RX) {{
+            array = {instance}_RX;
+            array_len = {instance}_RX_len;
+        }} else if(mode == TX) {{
+            array = {instance}_TX;
+            array_len = {instance}_TX_len;
+        }}
+    }}
+""")
+
+    f.write("""
+    for (uint8_t i = 0; i < array_len; i++) {
+        if(array[i].pin == pin) {
+            return array[i].AF;
+        }
+    }\n}\n\n""")
+
+
+def write_get_i2c_af(f, af_map):
+    f.write("uint8_t get_I2C_AF(I2C_HandleTypeDef* handle, Pin* pin, uint8_t mode) {\n")
+
+    write_af_arrays(f, af_map, "I2C")
+
+    f.write("    AF_Info* array;\n    uint8_t array_len;\n\n")
+
+    for peripheral in af_map['I2C'].keys():
+        if af_map['I2C'][peripheral]:
+            f.write(f"""
+    if(handle->Instance == {peripheral}) {{
+        if(mode == RX) {{
+            array = {peripheral}_RX;
+            array_len = {peripheral}_RX_len;
+        }} else if(mode == TX) {{
+            array = {peripheral}_TX;
+            array_len = {peripheral}_TX_len;
+        }}
+    }}
+""")
+
+    f.write("""
+    for (uint8_t i = 0; i < array_len; i++) {
+        if(array[i].pin == pin) {
+            return array[i].AF;
+        }
+    }\n}\n\n""")
+
+
+def write_get_fdcan_af(f, af_map):
+    f.write("uint8_t get_FDCAN_AF(FDCAN_HandleTypeDef* handle, Pin* pin, uint8_t mode) {\n")
+
+    write_af_arrays(f, af_map, "FDCAN")
+
+    f.write("    AF_Info* array;\n    uint8_t array_len;\n\n")
+
+    for peripheral in af_map['FDCAN'].keys():
+        if af_map['FDCAN'][peripheral]:
+            f.write(f"""
+    if(handle->Instance == {peripheral}) {{
+        if(mode == RX) {{
+            array = {peripheral}_RX;
+            array_len = {peripheral}_RX_len;
+        }} else if(mode == TX) {{
+            array = {peripheral}_TX;
+            array_len = {peripheral}_TX_len;
+        }}
+    }}
+""")
+
+    f.write("""
+    for (uint8_t i = 0; i < array_len; i++) {
+        if(array[i].pin == pin) {
+            return array[i].AF;
+        }
+    }\n}\n\n""")
+
+
+def write_uart_array(f, peripheral_map):
+    f.write("UART_Peripheral UART_Peripherals[] = {\n")
+
+    for bus in ("UART", "USART"):
+        for instance, values in peripheral_map.get(bus, {}).items():
+            if not values:
+                continue
+            if not values["RX"] or not values["TX"]:
+                continue
+
+            rx_mask = " | ".join(pin[:2] + "_" + pin[2:] + ".universal_mask" for pin in values["RX"])
+            tx_mask = " | ".join(pin[:2] + "_" + pin[2:] + ".universal_mask" for pin in values["TX"])
+
+            f.write(
+                f"    {{{instance}, ({rx_mask}), ({tx_mask}), NC, NC, false}},\n"
+            )
+
+    f.write("};\n\n")
+    f.write("const uint8_t UART_PERIPHERAL_COUNT = sizeof(UART_Peripherals) / sizeof(UART_Peripherals[0]);\n\n")
+
+
+def write_i2c_array(f, peripheral_map):
+    f.write("I2C_Peripheral I2C_Peripherals[] = {\n")
+
+    for instance, values in peripheral_map.get("I2C", {}).items():
+        if not values:
+            continue
+        if not values["SDA"] or not values["SCL"]:
+            continue
+
+        sda_mask = " | ".join(pin[:2] + "_" + pin[2:] + ".universal_mask" for pin in values["SDA"])
+        scl_mask = " | ".join(pin[:2] + "_" + pin[2:] + ".universal_mask" for pin in values["SCL"])
+
+        f.write(
+            f"    {{{instance}, ({sda_mask}), ({scl_mask}), NC, NC, false}},\n"
+        )
+
+    f.write("};\n\n")
+    f.write("const uint8_t I2C_PERIPHERAL_COUNT = sizeof(I2C_Peripherals) / sizeof(I2C_Peripherals[0]);\n\n")
+
+
+def write_fdcan_array(f, peripheral_map):
+    f.write("FDCAN_Peripheral FDCAN_Peripherals[] = {\n")
+
+    for instance, values in peripheral_map.get("FDCAN", {}).items():
+        if not values:
+            continue
+        if not values["RX"] or not values["TX"]:
+            continue
+
+        rxd_mask = " | ".join(pin[:2] + "_" + pin[2:] + ".universal_mask" for pin in values["RX"])
+        txd_mask = " | ".join(pin[:2] + "_" + pin[2:] + ".universal_mask" for pin in values["TX"])
+
+        f.write(
+            f"    {{{instance}, ({rxd_mask}), ({txd_mask}), NC, NC, false}},\n"
+        )
+
+    f.write("};\n\n")
+    f.write("const uint8_t FDCAN_PERIPHERAL_COUNT = sizeof(FDCAN_Peripherals) / sizeof(FDCAN_Peripherals[0]);\n\n")
 
 
 def create_cpp_file(file_path, peripheral_map, pin_map, af_map):
@@ -91,31 +257,10 @@ def create_cpp_file(file_path, peripheral_map, pin_map, af_map):
         # Includes
         f.write('#include "peripheralmap.h"\n#include "stm32h7xx_hal.h"\n\n')
 
-        # Array of UART peripherals
-        f.write("UART_Peripheral UART_Peripherals[] = {\n")
-        for peripheral, map in peripheral_map.items():
-            for instance, values in map.items():
-                if values['RX'] and values['TX']:
-                    f.write(f"    {{{instance}, (")
-                    for index, pin in enumerate(values['RX']):
-                        pin_name = pin[:2] + "_" + pin[2:]
-                        if index == len(values['RX']) - 1:
-                            f.write(f"{pin_name}.universal_mask), ")
-                        else:
-                            f.write(f"{pin_name}.universal_mask | ")
-                    
-                    f.write("(")
-                    for index, pin in enumerate(values['TX']):
-                        pin_name = pin[:2] + "_" + pin[2:]
-                        if index == len(values['TX']) - 1:
-                            f.write(f"{pin_name}.universal_mask), ")
-                        else:
-                            f.write(f"{pin_name}.universal_mask | ")
-                    f.write("NC, NC, false},\n")
-        f.write("};\n\n")
-
-        f.write("const uint8_t UART_PERIPHERAL_COUNT = sizeof(UART_Peripherals) / sizeof(UART_Peripherals[0]);\n\n")
-
+        # Peripheral arrays
+        write_uart_array(f, peripheral_map)
+        write_i2c_array(f, peripheral_map)
+        write_fdcan_array(f, peripheral_map)
 
         # GPIO clock enable function
         f.write("void gpio_clock_enable(GPIO_TypeDef* handle){\n")
@@ -132,38 +277,11 @@ def create_cpp_file(file_path, peripheral_map, pin_map, af_map):
                 f.write(f"    else if(handle == GPIO{block}){{\n        __HAL_RCC_GPIO{block}_CLK_ENABLE();\n    }}\n")
         f.write("    return;\n}\n\n")
 
+        # Alternate function methods
+        write_get_uart_af(f, af_map)
+        write_get_i2c_af(f, af_map)
+        write_get_fdcan_af(f, af_map)
 
-        # UART alternate function method
-        f.write("uint8_t get_UART_AF(UART_HandleTypeDef* handle, Pin* pin, uint8_t mode) {\n")
-        for peripheral, map in af_map.items():
-            for mode, pins in map.items():
-                if pins:
-                    f.write(f"    AF_Info {peripheral}_{mode}[] = {{\n")
-                    for pin_name, af in pins.items():
-                        pin_name = pin_name[:2] + "_" + pin_name[2:]
-                        f.write(f"        {{{pin_name}, {af}}},\n")
-                    f.write("    };\n\n")
-                    f.write(f"    uint8_t {peripheral}_{mode}_len = {len(pins)};\n\n")
-        
-        f.write("    AF_Info* array;\n\tuint8_t array_len;\n\n")
-
-        for peripheral, map in af_map.items():
-            f.write(f"""    if(handle == {peripheral}) {{
-        if(mode == RX) {{
-            array = {peripheral}_RX;
-            array_len = {peripheral}_RX_len;
-        }} else if(mode == TX) {{
-            array = {peripheral}_TX;
-            array_len = {peripheral}_TX_len;
-        }}
-    }}\n\n""")
-
-        f.write("""\tfor (uint8_t i = 0; i < array_len; i++) {
-        if(array[i].pin == pin) {
-            return array[i].AF;
-        }
-    }\n""")
-        f.write("}\n\n")
 
 if __name__ == "__main__":
     dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'input/STM32H743ZITx.xml')
