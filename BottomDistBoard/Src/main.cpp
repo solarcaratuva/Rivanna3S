7d59227952afdb9aad04a8828c32979127ff6426
TamerTimraz <tamertimraz04@gmail.com>
2026-02-12 17:24:20 -0500

    testing with David's SPI

--- BottomDistBoard/Src/main.cpp
+++ BottomDistBoard/Src/main.cpp
@@ -39,6 +39,7 @@
 #include "lock.h"
 #include "log.h"
 #include "SPI.h"
+#include "M95.h"
 
 extern "C" void app_main(void *argument)
 {
@@ -59,17 +60,53 @@ extern "C" void app_main(void *argument)
   }
   */
 
-  SPI spi4(PE_6, PE_5, PE_2, 100000);
+  // SPI spi4(PE_6, PE_5, PE_2, 100000);
 
-  uint8_t rx;
-  uint8_t tx;
+  // uint8_t rx;
+  // uint8_t tx;
 
-  while (1) {
-    // Read 1 byte
-    spi4.read(&rx, 1);
+  // while (1) {
+  //   // Read 1 byte
+  //   spi4.read(&rx, 1);
 
-    // Write it back
-    tx = rx;
-    spi4.write(&tx, 1);
+  //   // Write it back
+  //   tx = rx;
+  //   spi4.write(&tx, 1);
+  // }
+
+  uint8_t StatusRegisterValue = 0;
+	uint8_t ReadValue = 0;
+	uint8_t M95Type;
+	uint8_t TxData1 = 'D';
+	uint8_t TxData2 = 'E';
+	uint8_t TxData3 = 'F';
+
+  M95Type = M95040; DisableChipSelectM95(M95Type);
+
+  //Disable write-protect and hold
+  DisableWriteProtectM95();
+  DisableHoldM95();
+
+  //Turn off block protection
+  M95Type = M95040;
+  StatusRegisterValue = ReadStatusRegisterM95(M95Type);
+  StatusRegisterValue = StatusRegisterValue & ~(0x0C);
+  WriteStatusRegisterM95(M95Type, StatusRegisterValue);
+
+  while(1) {
+    M95Type = M95040;
+	  WriteByteM95(M95Type, 0x00000001, TxData1);
+	  StatusRegisterValue = 0;
+	  StatusRegisterValue = ReadStatusRegisterM95(M95Type);
+	  while (StatusRegisterValue & 0x01) {
+		  StatusRegisterValue = ReadStatusRegisterM95(M95Type);
+	  }
+	  ReadValue = 0;
+	  ReadValue = ReadByteM95(M95Type, 0x00000001);
+    log_debug("%c", ReadValue);
+
+	  if (TxData1 == ReadValue) {
+		  HAL_Delay(1);
+	  }
   }
 }
