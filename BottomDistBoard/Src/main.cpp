8e9ef07bf56744172ea9fa70488601f87f4035a6
TamerTimraz <tamertimraz04@gmail.com>
2026-02-22 14:21:30 -0500

    added M95 code into main.cpp

--- BottomDistBoard/Src/main.cpp
+++ BottomDistBoard/Src/main.cpp
@@ -39,7 +39,232 @@
 #include "lock.h"
 #include "log.h"
 #include "SPI.h"
-#include "M95.h"
+
+#define M95040		0
+#define M95256		1
+#define M95M04		2
+
+// SPI commands
+#define SPI_WREN	0x06
+#define SPI_WRDI	0x04
+#define SPI_RDSR	0x05
+#define SPI_WRSR	0x01
+#define SPI_READ	0x03
+#define SPI_WRITE	0x02
+#define SPI_RDID	0x83
+#define SPI_WRID	0x82
+#define SPI_RDLS	0x83
+#define SPI_LID		0x82
+
+SPI spi4(PE_6, PE_5, PE_2, 100000);
+
+
+void EnableChipSelectM95(uint8_t M95Type) //DONE
+{
+	switch (M95Type) {
+		case M95040:
+			HAL_GPIO_WritePin(GPIOF, GPIO_PIN_7, GPIO_PIN_RESET);
+			break;
+		// case M95256:
+		// 	HAL_GPIO_WritePin(U6_Sn_GPIO_Port, U6_Sn_Pin, GPIO_PIN_RESET);
+		// 	break;
+		// case M95M04:
+		// 	HAL_GPIO_WritePin(GPIOB, U7_Sn_Pin, GPIO_PIN_RESET);
+		// 	break;
+		default:
+			return;
+	}
+
+	return;
+}
+
+void DisableChipSelectM95(uint8_t M95Type)	//DONE
+{
+	switch (M95Type) {
+			case M95040:
+				HAL_GPIO_WritePin(GPIOF, GPIO_PIN_7, GPIO_PIN_SET);
+				break;
+			// case M95256:
+			// 	HAL_GPIO_WritePin(U6_Sn_GPIO_Port, U6_Sn_Pin, GPIO_PIN_SET);
+			// 	break;
+			// case M95M04:
+			// 	HAL_GPIO_WritePin(GPIOB, U7_Sn_Pin, GPIO_PIN_SET);
+			// 	break;
+			default:
+				return;
+		}
+}
+
+void DisableWriteProtectM95(void) //DONE
+{
+	//set the Write Protect pin high
+	HAL_GPIO_WritePin(GPIOF, GPIO_PIN_6, GPIO_PIN_SET);
+	return;
+}
+
+void DisableHoldM95(void) //DONE
+{
+	HAL_GPIO_WritePin(GPIOF, GPIO_PIN_5, GPIO_PIN_SET);
+	return;
+}
+
+void WriteEnableM95(uint8_t M95Type) //DONE
+{
+	uint8_t Command = SPI_WREN;
+	//Enable Chip Select
+	EnableChipSelectM95(M95Type);
+
+	//Send Command
+	//spi4.write(&Command);
+    spi4.write(&Command, 1);
+
+	//Disable Chip Select to complete command
+	DisableChipSelectM95(M95Type);
+
+}
+
+void WriteStatusRegisterM95(uint8_t M95Type, uint8_t WriteData)
+{
+	uint8_t Command = SPI_WRSR;
+
+	//Enable Write
+	WriteEnableM95(M95Type);
+
+	//Enable Chip Select
+	EnableChipSelectM95(M95Type);
+
+	//Send Command and Status Register State
+	spi4.write(&Command, 1);
+	spi4.write(&WriteData, 1);
+
+	//Disable Chip Select
+	DisableChipSelectM95(M95Type);
+
+
+}
+
+uint8_t ReadStatusRegisterM95(uint8_t M95Type)
+{
+    uint8_t Command = SPI_RDSR;
+	uint8_t ReturnValue = 0;
+    
+	//Enable Chip Select
+	EnableChipSelectM95(M95Type);
+    
+    log_debug("%s", "SR:1");
+	//Send Command & Recieve Data
+	spi4.write(&Command, 1);
+    log_debug("%s", "SR:2");
+	spi4.read(&ReturnValue, 1);
+    log_debug("%s", "SR:3");
+
+
+	//Disable Chip Select
+	DisableChipSelectM95(M95Type);
+
+	return ReturnValue;
+}
+
+void WriteByteM95(uint8_t M95Type, uint8_t Address, uint8_t WriteData)//DONE
+{
+
+	uint8_t Command = SPI_WRITE;
+
+	//Enable Write
+	WriteEnableM95(M95Type);
+
+	//Enable Chip Select
+	EnableChipSelectM95(M95Type);
+
+
+
+	switch (M95Type) {
+				case M95040:
+					Command = SPI_WRITE | ((Address & 0x100)>>5);
+					//Send Command, Address, & Data
+					spi4.write(&Command, 1);
+
+					spi4.write(&Address, 1);
+					break;
+				// case M95256:
+				// 	//Send Command, Address, & Data
+				// 	spi4.write(&Command, 1);
+
+				// 	spi4.write((uint8_t*) Address>>8, 1);
+				// 	spi4.write((uint8_t*) Address, 1);
+				// 	break;
+				// case M95M04:
+				// 	//Send Command, Address, & Data
+				// 	spi4.write(&Command, 1);
+
+				// 	spi4.write((uint8_t*) Address>>16, 1);
+				// 	spi4.write((uint8_t*) Address>>8, 1);
+				// 	spi4.write((uint8_t*) Address, 1);
+				// 	break;
+				default:
+					return;
+	}
+
+	spi4.write(&WriteData, 1);
+
+	//Disable Chip Select
+	DisableChipSelectM95(M95Type);
+
+
+}
+
+uint8_t ReadByteM95(uint8_t M95Type, uint8_t Address) //DONE
+{
+	uint8_t Command = SPI_READ;
+	uint8_t ReturnValue = 0;
+
+	switch (M95Type) {
+				case M95040:
+					//Enable Chip Select
+					EnableChipSelectM95(M95Type);
+					Command = SPI_READ | ((Address & 0x100)>>5); //add A8 bit to instruction
+					//Send commands and recieve
+					spi4.write(&Command, 1);
+					spi4.write(&Address, 1);
+					spi4.read(&ReturnValue, 1);
+
+					//Disable Chip Select
+					DisableChipSelectM95(M95Type);
+					break;
+
+				// case M95256:
+				// 	//Enable Chip Select
+				// 	EnableChipSelectM95(M95Type);
+
+				// 	spi4.write(&Command, 1);
+				// 	spi4.write(&(uint8_t) Address>>8, 1);
+				// 	spi4.write(&(uint8_t) Address, 1);
+				// 	spi4.read(&ReturnValue, 1);
+
+				// 	//Disable Chip Select
+				// 	DisableChipSelectM95(M95Type);
+				// 	break;
+				// case M95M04:
+				// 	//Enable Chip Select
+				// 	EnableChipSelectM95(M95Type);
+
+				// 	spi4.write(&Command, 1);
+				// 	spi4.write(&Address>>16, 1);
+				// 	spi4.write(&Address>>8, 1);
+				// 	spi4.write(&Address, 1);
+				// 	spi4.read(&ReturnValue, 1);
+
+				// 	//Disable Chip Select
+				// 	DisableChipSelectM95(M95Type);
+				// 	break;
+
+				default:
+					return 0;
+	}
+
+
+	return ReturnValue;
+}
 
 void app_main()
 {
@@ -59,7 +284,7 @@ void app_main()
   // }
   
 
-  // SPI spi4(PE_6, PE_5, PE_2, 100000);
+  //SPI spi4(PE_6, PE_5, PE_2, 100000);
 
   // uint8_t rx;
   // uint8_t tx;
@@ -74,11 +299,11 @@ void app_main()
   // }
 
   uint8_t StatusRegisterValue = 0;
-	uint8_t ReadValue = 0;
-	uint8_t M95Type;
-	uint8_t TxData1 = 'D';
-	uint8_t TxData2 = 'E';
-	uint8_t TxData3 = 'F';
+  uint8_t ReadValue = 0;
+  uint8_t M95Type;
+  uint8_t TxData1 = 'D';
+  uint8_t TxData2 = 'E';
+  uint8_t TxData3 = 'F';
 
   log_debug("%s", "ONE");
 
@@ -94,6 +319,7 @@ void app_main()
   M95Type = M95040;
   StatusRegisterValue = ReadStatusRegisterM95(M95Type);
   StatusRegisterValue = StatusRegisterValue & ~(0x0C);
+  log_debug("READ SR STATUS: %x", StatusRegisterValue);
   WriteStatusRegisterM95(M95Type, StatusRegisterValue);
 
   log_debug("%s", "THREE");
@@ -110,7 +336,7 @@ void app_main()
 	  }
 	  ReadValue = 0;
 	  ReadValue = ReadByteM95(M95Type, 0x00000001);
-    log_debug("%c", ReadValue);
+      log_debug("%c", ReadValue);
 
 	  if (TxData1 == ReadValue) {
 		  HAL_Delay(1);
