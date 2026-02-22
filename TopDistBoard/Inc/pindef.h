abcbc222dcc64ddf4fed86e12af4e76be0928bac
Ethan Mathias <ethanmathias@gmail.com>
2026-02-21 23:04:59 -0500

    top dist board mostly done needs review from colby

---
+++ TopDistBoard/Inc/pindef.h
@@ -0,0 +1,14 @@
+#define LEFT_TURN_EN NC
+#define RIGHT_TURN_EN NC
+#define HAZARD_EN NC
+#define STROBE_EN NC
+#define DRL_EN NC
+
+#define BRAKE_EN NC
+
+#define CAN_TX NC
+#define CAN_RX NC
+#define CAN_STANDBY NC
+
+#define LOG_TX NC
+#define LOG_RX NC
--- TopDistBoard/Src/main.cpp
+++ TopDistBoard/Src/main.cpp
@@ -20,37 +20,158 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "FreeRTOS.h"
-#include "task.h"
-#include "queue.h"
-#include "semphr.h"
-#include "timers.h"
-#include "pinmap.h"
-#include "peripheralmap.h"
 #include "DigitalIn.h"
 #include "DigitalOut.h"
-#include "UART.h"
 #include "AnalogIn.h"
-#include "Timeout.h"
 #include "Clock.h"
 #include "thread.h"
-#include "Timeout.h"
-#include "lock.h"
 #include "log.h"
+#include "Rivanna3SCanStructs.h"
+#include "MotorControllerCanStructs.h"
+#include "BPSCanStructs.h"
+#include "CanInterface.h"
+#include "pindef.h"
+
+DigitalOut left_turn_signal(LEFT_TURN_EN);
+DigitalOut right_turn_signal(RIGHT_TURN_EN);
+DigitalOut hazard_signal(HAZARD_EN);
+DigitalOut bps_strobe(STROBE_EN);
+DigitalOut brake_light(BRAKE_EN);
+
+bool flashLeftTurnSignal = false;
+bool flashRightTurnSignal = false;
+bool flashHazards = false;
+bool has_faulted = false; // true if there is any fault that locks the car until reset
+bool brake_from_pedal = false;
+bool brake_from_motor = false;
+
+const bool PIN_ON = true;
+const bool PIN_OFF = false;
+
+#define LOG_LEVEL LOG_DEBUG
+#define SIGNAL_FLASH_PERIOD 500
+
+CanInterface main_can = CanInterface(CAN_TX, CAN_RX, CAN_STANDBY, 250000, CanNetwork::Main);
+
+Thread signal_thread;
+
+// listen for DashboardCommands CAN message for turn signals and hazards
+ void handle_dashboard_commands(SerializedCanMessage &msg)
+{
+    DashboardCommands cmd{};
+    cmd.deserialize(&msg);
+
+    flashHazards = cmd.hazards;
+    flashLeftTurnSignal = cmd.left_turn_signal;
+    flashRightTurnSignal = cmd.right_turn_signal;
+    }
+
+
+// listen for PedalStatus and MotorCommands CAN message to set brake lights
+void handle_pedal_status(SerializedCanMessage &msg)
+{
+    PedalStatus status{};
+    status.deserialize(&msg);
+
+    brake_from_pedal = status.brake_pedal;
+    brake_light.write(brake_from_pedal || brake_from_motor);
+}
+
+void handle_motor_commands(SerializedCanMessage &msg)
+{
+    MotorCommands commands{};
+    commands.deserialize(&msg);
+
+    brake_from_motor = commands.regen_braking > 0;
+    brake_light.write(brake_from_pedal || brake_from_motor);
+}
+
+// listen for BpsError to set BPS strobe light
+//Ask colby what this BPS strobe light is?
+ void handle_bpsfault_messages(SerializedCanMessage &msg)
+{
+    BpsError status{};
+    status.deserialize(&msg);
+    if (status.has_active_fault())
+    {
+        has_faulted = true;
+        log_fault("BPS fault detected!");
+    }
+    bps_strobe.write(status.has_active_fault() ? PIN_ON : PIN_OFF);
+}
+
+// listen for all faults to set hazards
+void handle_contactor_fault(SerializedCanMessage &msg)
+{
+    Contactor12Error status{};
+    status.deserialize(&msg);
+
+    if (status.has_active_fault())
+    {
+        has_faulted = true;
+        log_fault("Contactor fault detected!");
+    }
+}
+
+void handle_motor_controller_fault(SerializedCanMessage &msg)
+{
+    MotorControllerError status{};
+    status.deserialize(&msg);
+
+    if (status.has_active_fault())
+    {
+        has_faulted = true;
+        log_fault("Motor controller fault detected!");
+    }
+}
+
+
+// turn signal flash handler
+void signal_flash_handler()
+{
+    Clock signal_flash_clock;
+    
+    while (1) {
+        if (flashHazards || has_faulted) {
+            left_turn_signal.write(!left_turn_signal.read());
+            right_turn_signal.write(left_turn_signal.read());
+            hazard_signal.write(left_turn_signal.read());
+        } else if (flashLeftTurnSignal) {
+            left_turn_signal.write(!left_turn_signal.read());
+            right_turn_signal.write(PIN_OFF);
+            hazard_signal.write(PIN_OFF);
+        } else if (flashRightTurnSignal) {
+            right_turn_signal.write(!right_turn_signal.read());
+            left_turn_signal.write(PIN_OFF);
+            hazard_signal.write(PIN_OFF);
+        } else {
+            left_turn_signal.write(PIN_OFF);
+            right_turn_signal.write(PIN_OFF);
+            hazard_signal.write(PIN_OFF);
+        }
+
+        signal_flash_clock.sleep_since(SIGNAL_FLASH_PERIOD);
+    }
+}
+
+
+
+
 
 void app_main()
 {
 
-  /* USER CODE BEGIN Init */
-  log_configure(DEBUG_LVL, PD_8, PD_9, 921600);
-  /* USER CODE END Init */
+    log_configure(INFO_LVL, LOG_TX, LOG_RX, 250000);
+    log_info("Top Dist Board starting up...");
 
-  DigitalOut LED1(PB_0);
+    signal_thread.start(signal_flash_handler);
 
-  while (1)
-  {
-    log_debug("%s","HERE");
-    HAL_Delay(1000);
-    LED1.write(!LED1.read());
-  }
+    main_can.register_callback(DashboardCommands::get_message_ID(), handle_dashboard_commands);
+    main_can.register_callback(PedalStatus::get_message_ID(), handle_pedal_status);
+    main_can.register_callback(MotorCommands::get_message_ID(), handle_motor_commands);
+    main_can.register_callback(BpsError::get_message_ID(), handle_bpsfault_messages);
+    main_can.register_callback(Contactor12Error::get_message_ID(), handle_contactor_fault);
+    main_can.register_callback(MotorControllerError::get_message_ID(), handle_motor_controller_fault);
+
+    log_info("Top Dist Board initialized");
 }
