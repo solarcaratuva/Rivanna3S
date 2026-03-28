1c4b21a15edece515600882ca51f6cc48961589a
abhinavp5 <abhinavpappu05@gmail.com>
2026-03-28 16:50:24 -0400

    implementatino of SD card class and working log  testing

--- TelemetryBoard/Src/main.cpp
+++ TelemetryBoard/Src/main.cpp
@@ -1,46 +1,27 @@
 #include "log.h"
 #include "CanInterface.h"
 #include "pindef.h"
-#include "SPI.h"
-#include "fatfs.h"
-
+#include "../../Common/Drivers/Inc/SPI.h"
+#include "SD.h"
+#include "task.h"
 
 #define LOG_LEVEL DEBUG_LVL
 
 CanInterface main_can = CanInterface(CAN_TX, CAN_RX, CAN_STBY, 250000, CanNetwork::Main);
 
-
- void handle_all_messages(const SerializedCanMessage &msg) {
+void handle_all_messages(const SerializedCanMessage &msg) {
     // 1. send message over radio
     // 2. send message over LTE
 }
 
-
 void app_main() {
     log_configure(LOG_LEVEL, LOG_TX, LOG_RX, 250000);
+    log_info("Telemetry Board starting up");
     log_info("Telemetry Board starting up...");
-
-
-    // Temporary Write Test
-    MX_FATFS_Init();
-    uint8_t sd_card_write_result = FATFS_SD_TestWrite();
-    switch (sd_card_write_result){
-    case 0:
-        log_debug("Succesfully Wrote!");
-        break;
-    case 1:
-        log_debug("Failed to Mount!");
-        break;
-    case 2:
-        log_debug("Failed to Open/Create!");
-        break;
-    case 3:
-        log_debug("Failed to Write bytes!");
-        break;
-    }
     
-
+    static SPI sd_spi(SPI2_MOSI, SPI2_MISO, SPI2_SCK, 400000);
+    static SdCard sd_card(&sd_spi);
+    sd_card.attach_to_log();
+    
     main_can.register_always_callback(handle_all_messages);
-
-    log_info("Telemetry Board initialized");
 }
