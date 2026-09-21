c8ff96e8520ec5c94ae04f11a4502b99e0458341
Ethan Mathias <ethanmathias@gmail.com>
2026-09-21 14:04:17 -0400

    comment clean up

--- TelemetryBoard/Src/main.cpp
+++ TelemetryBoard/Src/main.cpp
@@ -62,8 +62,9 @@ void radio_send(const SerializedCanMessage &msg)
 }
 
 void handle_all_messages(const SerializedCanMessage &msg) {
-    // 1. send message over radio
+    // Check for faults
     FaultHandler::check_for_any_faults(msg);
+    // 1. send message over radio
     radio_send(msg);
     // 2. send message over LTE
 
